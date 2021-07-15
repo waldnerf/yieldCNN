fix: change year-1 and normalising histograms

preprocess_2d_inputs.py
@@ -39,7 +39,7 @@ def retain_regions(df, groups, key, target, cum=0.9):
         my_list = my_list + list(df_i[target])
     return list(set(my_list))
 
-def get_2D_histogram(df, unit, year, ts_length, ts_start):
+def get_2D_histogram(df, unit, year, ts_length, ts_start, normalise=True):
     binDict = {
         'NDVI': {'min': 0.05, 'range': 0.85, 'n': 64},
         'rad': {'min': 40000, 'range': 280000, 'n': 64},
@@ -60,15 +60,20 @@ def get_2D_histogram(df, unit, year, ts_length, ts_start):
         yValues = binValues[0:-1]+(binValues[1]-binValues[0])/2
         histo_cols = [col for col in df_var.columns if 'cls_cnt' in col]
         histo = df_var[histo_cols].to_numpy().transpose()
+
         start_sel = np.where([x == f'{year}{ts_start}' for x in xValues])[0][0]
         histo_year = histo[:, start_sel:(start_sel+ts_length)]
+        if normalise:
+            histo_max = histo_year.max(axis=0)
+            histo_min = histo_year.min(axis=0)
+            histo_year = (histo_year - histo_min) / (histo_max - histo_min)
+
         arr_out.append(histo_year)
 
     arr_out = np.stack(arr_out, axis=2)
     return arr_out
 
 def main(fn_features, fn_stats, fn_out=''):
-
     df_stats = pd.read_csv(fn_stats)
     df_stats = df_stats[['Year', 'Area', 'Yield', 'Production', 'AU_name',  'ASAP1_ID', 'Crop_name']].copy()
     df_stats['Crop_name'] = df_stats['Crop_name'].apply(lambda x: x.replace(' ', ''))
@@ -76,7 +81,6 @@ def main(fn_features, fn_stats, fn_out=''):
     df_filter = df_stats.groupby(['ASAP1_ID', 'AU_name', 'Crop_name']).agg({'Production': 'mean'}).reset_index()
     region_ids = retain_regions(df_filter, groups='Crop_name', key='Production', target='ASAP1_ID')
     df_stats = df_stats.loc[df_stats['ASAP1_ID'].isin(region_ids), :].copy()
-
 
     df_statsw = df_stats.pivot_table(index=['ASAP1_ID', 'AU_name',  'Year'],
                                      columns=['Crop_name'],
@@ -99,9 +103,10 @@ def main(fn_features, fn_stats, fn_out=''):
     sfig_dir = cst.my_project.figs_dir / '2D_inputs'
     sfig_dir.mkdir(parents=True, exist_ok=True)
     for i, row in df_statsw.iterrows():
-        hist = get_2D_histogram(df_raw, unit=int(row['ASAP1_ID']), year=int(row['Year']), ts_length=36, ts_start='1001')
-        super_title = f'{row["AU_name"]} ({row["Year"]}) - barley {round(row["Yield_Barley"], 2)} t/ha,' \
-                      f' soft wheat {round(row["Yield_Softwheat"], 2)} t/ha, ' \
+        # Start of season is at year -1 !!!
+        hist = get_2D_histogram(df_raw, unit=int(row['ASAP1_ID']), year=int(row['Year'])-1, ts_length=36, ts_start='1001')
+        super_title = f'{row["AU_name"]} ({row["Year"]}) - barley {round(row["Yield_Barley"], 2)} t/ha, ' \
+                      f'soft wheat {round(row["Yield_Softwheat"], 2)} t/ha, ' \
                       f'durum wheat {round(row["Yield_Durumwheat"], 2)} t/ha'
         fig_name = sfig_dir / f'{row["AU_name"]}_{row["Year"]}_2Dinputs.png'
         plot_2D_inputs_by_region(hist, variables, super_title, fig_name=fig_name)