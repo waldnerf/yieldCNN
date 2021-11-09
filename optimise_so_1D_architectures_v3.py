921b04de35412d84fc0e7162708d058fa42046d3
meronmi <michele.meroni@gmail.com>
2021-11-09 11:58:37 +0100

    for aws

--- optimise_so_1D_architectures_v3.py
+++ optimise_so_1D_architectures_v3.py
@@ -26,7 +26,7 @@ import datetime
 import sits.data_generator_1D2D as data_generator
 
 # global vars
-version = 'test_weights_int_0'
+version = 'v5'
 N_CHANNELS = 4  # -- NDVI, Rad, Rain, Temp
 dict_train_params = {
     'optuna_metric': 'rmse', #'rmse' or 'r2'
